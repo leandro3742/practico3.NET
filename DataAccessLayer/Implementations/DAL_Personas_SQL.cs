057904226fcd48828cb9a13389d11df386034707
leandro marrero <leandro.marrero03@gmail.com>
2022-08-30 20:38:16 -0300

    first commit

--- DataAccessLayer/Implementations/DAL_Personas_SQL.cs
+++ DataAccessLayer/Implementations/DAL_Personas_SQL.cs
@@ -11,13 +11,13 @@ namespace DataAccessLayer.Implementations
 {
     public class DAL_Personas_SQL : IDAL_Personas
     {
-        private string sqlConnection = "Server=localhost,14330;Database=Practico3;User Id=sa;Password=Tisj*2022;";
+        private string sqlConnection = "Server=DESKTOP-L7LID20\\SQLEXPRESS,1433;Database=Practico3;User id=sa;Password=1234;";
 
         public Persona AddPersona(Persona x)
         {
             using (var connection = new SqlConnection(sqlConnection))
             {
-                SqlCommand cmd = new SqlCommand("INSERT INTO PERSONAS VALUES(@nombre, @documento)", connection);
+                SqlCommand cmd = new SqlCommand("INSERT INTO PERSONA VALUES(@nombre, @documento)", connection);
                 {
                     cmd.Parameters.Add(new SqlParameter("nombre", x.Nombre));
                     cmd.Parameters.Add(new SqlParameter("documento", x.Documento));
@@ -32,12 +32,67 @@ namespace DataAccessLayer.Implementations
 
         public Persona Get(string documento)
         {
-            throw new NotImplementedException();
+            Persona result = null;
+
+            using (SqlConnection connection = new SqlConnection(sqlConnection))
+            {
+                string query = "SELECT Nombre, Documento FROM Persona WHERE Documento = @documento";
+                SqlCommand com = new SqlCommand(query, connection);
+                com.Parameters.AddWithValue("@documento", documento);
+
+                connection.Open();
+                SqlDataReader Reader = com.ExecuteReader();
+
+                if (Reader.Read())
+                {
+                    result = new Persona();
+
+                    result.Documento = Reader["Documento"].ToString();
+                    result.Nombre = Reader["Nombre"].ToString();
+
+                    Console.WriteLine(result.Nombre);
+
+                }
+                else
+                {
+                    Console.WriteLine("no llegamos");
+                }
+            }
+
+            return result;
         }
 
         public List<Persona> GetPersonas()
         {
-            throw new NotImplementedException();
+
+            List<Persona> result = null;
+
+            using (var connection = new SqlConnection(sqlConnection))
+            {
+                result = new List<Persona>();
+                string query = "SELECT * FROM Persona";
+                SqlCommand com = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader Reader = com.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    if (result == null)
+                    {
+                        result = new List<Persona>();
+                    }
+
+                    Persona P = new Persona();
+
+                    P.Documento = Reader["documento"].ToString();
+                    P.Nombre = Reader["Nombre"].ToString();
+
+                    result.Add(P);
+                }
+
+            }
+            return result;
         }
     }
 }
